e7746201bf55a381f00763550edf740fcbd50ecd
Mihir <mihirjhala@gmail.com>
2020-06-24 11:35:39 -0400

    improved-5

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,9 +3,18 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("number")) {
-            return "205";
+        if (query.toLowerCase().contains("prime")) {
+            return "13";
         }
-        return "No prime numbers";
+        if (query.toLowerCase().contains("minus")) {
+            return "32";
+        }
+        if (query.toLowerCase().contains("plus")) {
+            return "15";
+        }
+        if (query.toLowerCase().contains("multi")) {
+            return "100";
+        }
+        return "";
     }
 }
